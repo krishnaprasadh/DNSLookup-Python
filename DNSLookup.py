4aed5ae540640bde5f577ae55ca7bb5034266348
krishnaprasadh <rkpamb@gmail.com>
2016-12-28 02:28:50 +0530

    Add files via upload

---
+++ DNSLookup.py
@@ -0,0 +1,18 @@
+import socket
+import sys
+print "#" * 50
+print "DNSLOOKUP --Krishnaprasadh"
+print "#" * 50
+
+try:
+   for n in range (1,100):
+	address1 = raw_input("Enter address: ")
+	addr1 = socket.gethostbyname(address1)
+	print "DNSlookup for", address1, "is", addr1
+	print "Press ctrl+C to exit"
+
+except KeyboardInterrupt:
+	print "\n Exiting \n"
+	sys.exit()
+
+
